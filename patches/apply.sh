872cfc7a3f1ca78c76211102538794a884d9eb9a
ReallySnow <2569496392@qq.com>
2018-07-14 18:01:04 +0800

    AEX:reback to 29 patch , fix bootanimation and add Magisk

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-echo Install First Patches
+echo Install First Patches(sepolicy)
 cd ../../../../
 cd system/sepolicy
 git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tit/patches/0001-Revert-back-to-version-29.patch
 cd ../..
-echo Install Second Patches
+echo Install Second Patches(core)
 cd system/core
 git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tit/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
 git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tit/patches/system_core_libnetutils.patch
 cd ../..
-echo Install Third Patches
+echo Install Third Patches(frameworks)
 cd frameworks/native
 git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tit/patches/frameworks_native_ui.patch
 cd ../..
-echo Install Last Patches
+echo Install Last Patches(netd)
 cd system/netd
-git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tit/patches/system_netd.patch
 git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tit/patches/0001-Revert-Don-t-start-tethering-if-IPv6-RPF-is-not-supp.patch
 git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tit/patches/0002-Enable-Tethering.patch
 cd ../..
