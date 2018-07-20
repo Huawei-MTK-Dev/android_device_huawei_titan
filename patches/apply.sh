5c14b6dc3cebb4d23c77193810b65a726054705b
初羽 <31167977+ReallySnow@users.noreply.github.com>
2018-07-20 18:39:46 +0800

    fix sh

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-echo Install First Patches(sepolicy)
+echo Install First Patches
 cd ../../../../
 cd system/sepolicy
 git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tit/patches/0001-Revert-back-to-version-29.patch
 cd ../..
-echo Install Second Patches(core)
+echo Install Second Patches
 cd system/core
 git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tit/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
 git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tit/patches/system_core_libnetutils.patch
 cd ../..
-echo Install Third Patches(frameworks)
+echo Install Third Patches
 cd frameworks/native
 git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tit/patches/frameworks_native_ui.patch
 cd ../..
-echo Install Last Patches(netd)
+echo Install Last Patches
 cd system/netd
 git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tit/patches/0001-Revert-Don-t-start-tethering-if-IPv6-RPF-is-not-supp.patch
 git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tit/patches/0002-Enable-Tethering.patch
