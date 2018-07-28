7654b6282e4eeb3c5690801f474eb07a24d79073
ReallySnow <2569496392@qq.com>
2018-07-28 20:48:56 +0800

    tit:fix flash zip fstab error

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,4 +5,4 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/TCL/Escape_CM/AndroidBoardVendor.mk
+-include vendor/huawei/tit/AndroidBoardVendor.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,4 @@
 #used to add device to Omni's lunch
+add_lunch_combo lineage_tit_tl00-eng
+add_lunch_combo lineage_tit_tl00-user
 add_lunch_combo lineage_tit_tl00-userdebug
